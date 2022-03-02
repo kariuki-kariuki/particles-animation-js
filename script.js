9e1d100a8592d3f06d442115ccd81c10a05541de
Martin <mkmartinoes@gmail.com>
2022-03-02 17:50:34 +0300

    ft: handle mouse events

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let particleArray = [];
+
+
+// mouse event handler
+
+const mouse = {
+    x: null,
+    y: null,
+    radius: 160
+}
+
+window.addEventListener("mousemove", function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse.x, mouse.y);
+});
